cab8458b76166d56ef109ceb86d8af80b66f3a2e
songdaochuanshu <songdaochuanshu@gmail.com>
2022-02-15 15:18:49 +0800

    update gitgnore

---
+++ pages/about/index1.page.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: MiKin
+ * @Date: 2022-02-15 15:06:13
+ * @LastEditors: MiKin
+ * @LastEditTime: 2022-02-15 15:07:14
+ * @FilePath: \vite-ssr-project\pages\about\index1.page.vue
+-->
+<template>
+  <h1>Welcome</h1>
+  This page is:
+  <ul>
+    <li>Rendered to HTML.</li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+</script>
